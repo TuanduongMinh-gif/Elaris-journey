312dc0c231ca301992b43b43970eed54377d1e25
TuanduongMinh-gif <dmt101048@gmail.com>
2025-04-24 01:51:08 +0700

    Delete AudioManager.ts

--- AudioManager.ts
+++
@@ -1,3 +0,0 @@
-import { Howl } from 'howler';
-class AudioManager{sounds={};load(id,src){this.sounds[id]=new Howl({src:[src]});}play(id){this.sounds[id]?.play();}}
-export const audio=new AudioManager();
