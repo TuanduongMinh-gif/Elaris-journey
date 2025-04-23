f1e8bcd3aca7b5c311de321a3626cfd887ab614b
TuanduongMinh-gif <dmt101048@gmail.com>
2025-04-24 01:51:18 +0700

    Delete NotesEditor.tsx

--- NotesEditor.tsx
+++
@@ -1,6 +0,0 @@
-import React,useState from 'react';
-export default function NotesEditor(){
-  const [note,setNote]=useState('');
-  return <textarea className="w-full h-32 p-2 border" placeholder="Ghi chú..." 
-    value={note} onChange={e=>setNote(e.target.value)}/>;
-}
